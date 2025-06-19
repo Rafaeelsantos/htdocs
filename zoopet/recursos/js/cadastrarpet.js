f40ea536d7ce491c16f1e88e9df0bfdf40a6f8f2
Rafael Santos <126109568+Rafaeelsantos@users.noreply.github.com>
2025-06-18 21:52:38 -0300

    Delete zoopet directory

--- zoopet/recursos/js/cadastrarpet.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('mostrarMais').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('.aviso').classList.toggle('visivel');
-    if (document.querySelector('.aviso').classList.contains('visivel')) {
-        document.getElementById('mostrarMais').textContent = 'Ocultar texto';
-    } else {
-        document.getElementById('mostrarMais').textContent = 'Visualizar mais';
-    }
-});
